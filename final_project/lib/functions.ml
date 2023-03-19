a9b11c2fba6f15d5852c5029d2a20b03fb8396d2
RachJiangtj <tj232@cornell.edu>
2023-03-19 17:18:27 -0400

    finish print txt funciton

--- final_project/lib/functions.ml
+++ final_project/lib/functions.ml
@@ -1,1 +1,10 @@
-let a = 3
+let read_lines file process =
+  let in_ch = open_in file in
+  let rec read_line () =
+    let line = try input_line in_ch with End_of_file -> exit 0
+    in (* process line in this block, then read the next line *)
+       process line;
+       read_line ();
+in read_line ();;
+
+read_lines "trial.txt" print_endline;;
