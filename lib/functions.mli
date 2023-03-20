d5d1227a476e624e8480d7fdfd7a5bc01201783b
Haoxuan Zou <hz252@cornell.edu>
2023-03-19 21:50:39 -0400

    Update functions.mli

--- lib/functions.mli
+++ lib/functions.mli
@@ -1,7 +1,7 @@
 (** this file represent the data stored in functions.ml*)
 
 type event
-(** an object with type info represents an input record an event represents a
+(** an object with type event represents an input record an event represents a
     single event. It includes:
 
     - name of the event (string)
@@ -17,11 +17,15 @@ val summary : event list -> string
 (** [summary lst] would print a string of all info in lst, each info separated
     by a comma *)
 
+(** help_function takes a function name and return the spec of that function 
+    if it exists. *)
+(* val help_function : string -> string  *)
+
 (** event_to_string takes in an event an outputs a string that 
     contains all the information in this event. *)
 (* val event_to_string: event -> string *)
 
-(**input_event takes four parameters: 
+(** input_event takes four parameters: 
     name of the event (string);
     name of the payer (string);
     event participants' names (string list);
@@ -32,10 +36,17 @@ val summary : event list -> string
 (** print_all_event outputs all the events that the user input before. *)
 (* val print_all_event: _ -> event list *)
 
-(** name_search_event takes the name of the event and returns that 
+(** name_search_event takes the name (OR ID?? TBD) of the event and returns that 
     event if it exists. If it doesn't exist, it will return an 
     empty event (format TBD).*)
 (* val name_search_event: string -> event  *)
+
+(** delete_event takes the name (OR ID?? TBD) of the event and delete 
+    the corresponding event if it exists. It returns the event if it is 
+    deleted or an empty event if it does not exist.  *)
+(* val delete_event: string -> event; *)
+
+
 
 
 
