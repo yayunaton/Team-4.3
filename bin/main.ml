95a15d167f5e6b7c48b2e12832956666bf3bdb6c
JamesMac <zl293@cornell.edu>
2023-03-19 20:33:40 -0400

    First version of main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World!"
+print_endline
+  "Welcome to BillBook! To acquire information of a user, type in 'user \
+   username'. To acquire information of an event, type in 'event event_id'. \
+   For more information of functionalities type in 'help'. "
+
+let first_input = String.split_on_char ' ' (String.trim (input_line stdin))
+
+let action =
+  if first_input = [ "quit" ] then exit 0
+  else if List.hd first_input = "user" then
+    print_endline "This functionality is to be implemented"
+  else if List.hd first_input = "event" then
+    print_endline "This functionality is to be implemented"
--- lib/functions.ml
+++ lib/functions.ml
@@ -1,17 +1,25 @@
+type info = { a : string }
 
-type info = {a:string}
-let rec summary (b:info list) = match  b with
-| [] -> "none"
-| [h] -> (match h with {a} -> a)
-| h::t -> (match h with {a} -> a)^summary t
+let rec summary (b : info list) =
+  match b with
+  | [] -> "none"
+  | [ h ] -> (
+      match h with
+      | { a } -> a)
+  | h :: t ->
+      (match h with
+      | { a } -> a)
+      ^ summary t
 
 let read_lines file process =
   let in_ch = open_in file in
   let rec read_line () =
-    let line = try input_line in_ch with End_of_file -> exit 0
-    in (* process line in this block, then read the next line *)
-       process line;
-       read_line ();
-in read_line ();;
+    let line = try input_line in_ch with End_of_file -> exit 0 in
+    (* process line in this block, then read the next line *)
+    process line;
+    read_line ()
+  in
+  read_line ()
+;;
 
-read_lines "trial.txt" print_endline;;
+read_lines "trial.txt" print_endline
