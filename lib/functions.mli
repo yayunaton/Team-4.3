88ff622d64f5960de7eff31ac05fc185c1f5a753
RachJiangtj <tj232@cornell.edu>
2023-03-19 20:30:28 -0400

    implement the interface

--- lib/functions.mli
+++ lib/functions.mli
@@ -1,10 +1,15 @@
 (** this file represent the data stored in functions.ml*)
 
-
-type info 
+type info
 (** an object with type info represents an input record*)
 
 val summary : info list -> string
-(** [summary lst] would print a string of all info in lst, each info separated
+(** [summary lst] would output a string of all info in lst, each info separated
     by a comma *)
 
+val description : int -> string
+(** [description id] would output the descrption of info whose event ID is [id],
+    if the event is not found, return "False"*)
+
+val delete : int -> string
+(** [delete id] would output "Event deleted + [the event description]"*)
