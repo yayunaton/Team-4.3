83ec5d72f13a13b21bf0b28efa2b1881788a0cb8
Haoxuan Zou <hz252@cornell.edu>
2023-03-19 21:43:55 -0400

    Update functions.mli

--- lib/functions.mli
+++ lib/functions.mli
@@ -17,10 +17,26 @@ val summary : event list -> string
 (** [summary lst] would print a string of all info in lst, each info separated
     by a comma *)
 
+(** event_to_string takes in an event an outputs a string that 
+    contains all the information in this event. *)
+(* val event_to_string: event -> string *)
+
+(**input_event takes four parameters: 
+    name of the event (string);
+    name of the payer (string);
+    event participants' names (string list);
+    bill amount (float),
+    and outputs the event that is created. *)
 (* val input_event: string -> string -> string list -> float -> event *)
 
+(** print_all_event outputs all the events that the user input before. *)
 (* val print_all_event: _ -> event list *)
 
+(** name_search_event takes the name of the event and returns that 
+    event if it exists. If it doesn't exist, it will return an 
+    empty event (format TBD).*)
 (* val name_search_event: string -> event  *)
 
-(* val event_to_string: event -> string *)
+
+
+
