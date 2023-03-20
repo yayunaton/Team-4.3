067aff821d80d171e0665e29f5ba0bda48f76fc5
JamesMac <zl293@cornell.edu>
2023-03-19 22:10:02 -0400

    Add options for input_event and help_function

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,14 @@ print_endline "To acquire information of a user, type in 'user username'.";
 print_endline "To acquire information of an event, type in 'event event_id'.";
 print_endline "For more information of functionalities type in 'help'. "
 
+open Functions
+
 let first_input = String.split_on_char ' ' (String.trim (input_line stdin))
+
+(*let take_event = let a = String.trim (input_line stdin) in let b = String.trim
+  (input_line stdin) in let c = String.split_on_char ' ' (String.trim
+  (input_line stdin)) in let d = int_of_string (input_line stdin) in input_event
+  a b c d*)
 
 let action first_input =
   if first_input = [ "quit" ] then exit 0
@@ -12,5 +19,8 @@ let action first_input =
     print_endline "This functionality is to be implemented"
   else if List.hd first_input = "event" then
     print_endline "This functionality is to be implemented"
+(*else if List.hd first_input = "new event" then take_event else if List.hd
+  first_input = "help" then if List.length first_input = 1 then help_function ""
+  else help_function (List.nth first_input 1)*)
 
 let () = action first_input
