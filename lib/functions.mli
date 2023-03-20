8b422387d13c3758493375c1f38ac2289d3f1874
Haoxuan Zou <hz252@cornell.edu>
2023-03-19 21:59:14 -0400

    Wrote the function header and spec for all necessary functions

--- lib/functions.mli
+++ lib/functions.mli
@@ -46,8 +46,13 @@ val summary : event list -> string
     deleted or an empty event if it does not exist.  *)
 (* val delete_event: string -> event; *)
 
+(** debt_summary returns the current debt record of all participants 
+in the format of "who owes who how much money".*)
+(* val debt_summary: _ -> string *)
 
-
+(** name_search_debt takes in a person's name and outputs the debt 
+    record of that person (if the person exists) or an empty string. *)
+(* val name_search_debt: string -> string *)
 
 
 
